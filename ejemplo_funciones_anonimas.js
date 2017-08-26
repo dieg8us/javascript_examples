ea8c6a55b876dc1d2eef887d5543cfdc1d24a261
diegous <diego.uribe.segura@gmail.com>
2017-08-26 16:05:23 -0300

    ejemplo de funciones anomimas

--- ejemplo_funciones_anonimas.js
+++ ejemplo_funciones_anonimas.js
@@ -1,4 +1,41 @@
 
-var a = 10;
+// funcion anomina siempre en parenteris
+(function(){
 
-function
+  var a = 10;
+
+  // valor a antes de funcion
+  console.log( a );
+
+  function cambiarA(){
+    a = 20;
+  }
+
+  cambiarA();
+
+  // valor a despues de funcion
+  console.log( a );
+
+})();
+
+// otro ejemplo de funcion anomina
+
+function ejecutarFuncion( fn ){
+  // se ejecuta la funcion
+  if ( fn() === 1) {
+    return true;
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+console.log(
+  // se ejecuta funcion y se pasa una funcion anonima como
+  // parametro
+  ejecutarFuncion(function() {
+    console.log( "funcion anónima ejecutada!!!" );
+    return 1;
+  })
+);
